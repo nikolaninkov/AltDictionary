3347d4e34e6928610ff940a82f3a3bfcc6be1e4e
Nikola Ninkov <nikolaninkov@hotmail.com>
2022-03-21 00:52:17 +0100

    Basic interface implementation complete.

---
+++ AltDictionary/AltDictionary.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Alt
+{
+    public class AltDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        public AltDictionary()
+        {
+            collection = new List<KeyValuePair<TKey, TValue>>();
+            keys = new List<TKey>();
+            values = new List<TValue>();
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                TValue? value;
+                TryGetValue(key, out value);
+                return value;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    if (Remove(key))
+                    {
+                        collection.Add(new KeyValuePair<TKey, TValue>(key, value));
+                    }
+                    else
+                    {
+                        throw new KeyNotFoundException("There is no key-value pair with such a key.");
+                    }
+
+                }
+                else
+                {
+                    throw new ArgumentNullException("Key value cannot be null.");
+                }
+            }
+        }
+
+        public ICollection<TKey> Keys => keys;
+
+        public ICollection<TValue> Values => values;
+
+        public int Count => collection.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("Key value cannot be null.");
+            }
+            if (ContainsKey(key))
+            {
+                throw new ArgumentException("A value with that key already exists.");
+            }
+            collection.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            collection.Add(item);
+        }
+
+        public void Clear()
+        {
+            collection.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return collection.Contains(item);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("Key value cannot be null.");
+            }
+            return collection.Any(x => EqualityComparer<TKey>.Default.Equals(x.Key, key));
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("Array value cannot be null.");
+            }
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException("Array index must be a non-negative integer.");
+            }
+            if (array.Length - arrayIndex < collection.Count)
+            {
+                throw new ArgumentException("Array cannot hold all elements.");
+            }
+            int i = arrayIndex;
+            foreach (KeyValuePair<TKey, TValue> item in collection)
+            {
+                array[i++] = item;
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("Key value cannot be null.");
+            }
+            return collection.RemoveAll(x => EqualityComparer<TKey>.Default.Equals(x.Key, key)) > 0;
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return collection.Remove(item);
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentNullException("Key value cannot be null.");
+            }
+            KeyValuePair<TKey, TValue> element = collection.Find(x => EqualityComparer<TKey>.Default.Equals(x.Key, key));
+            value = element.Value;
+            return EqualityComparer<KeyValuePair<TKey, TValue>>.Default.Equals(element, default) ? false : true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        private List<KeyValuePair<TKey, TValue>> collection;
+        private List<TKey> keys;
+        private List<TValue> values;
+    }
+}
--- AltDictionary/Program.cs
+++ AltDictionary/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Alt;
+
+var dict = new AltDictionary<int, string>();
+dict.Add(1, "Jedan");
+dict.Add(2, "Dva");
+dict.Add(3, "Tri");
+Console.WriteLine(dict[1]);
+Console.WriteLine(dict[2]);
+Console.WriteLine(dict[3]);
